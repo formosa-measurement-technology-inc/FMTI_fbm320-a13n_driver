3a8cacaeced8aca894f8861df6c07928d8cdc9db
Conrad <conradalf@gmail.com>
2020-04-07 09:21:58 +0800

    Code modification of main_example.c

--- main_example.c
+++ main_example.c
@@ -1,11 +1,11 @@
 
 #include <stdio.h>
-#include <fbm320_a13n.>
+#include <fbm320_a13n.h>
 
 /* Private variable */
-int32_t real_p, real_t, altitude;
-volatile uint32_t TMR0_Ticks = 0; //one tick per millisecond(ms)
-volatile uint32_t fbm320_update_rdy = 0;
+int32_t real_p, real_t;
+extern volatile uint32_t TMR0_Ticks = 0; //one tick per millisecond(ms)
+extern volatile uint32_t fbm320_update_rdy = 0;
 
 /**
  * @brief      A timer generate an interrupt every millisecond
@@ -26,16 +26,15 @@ void TMR0_IRQHandler(void)
 /*---------------------------------------------------------------------------------------------------------*/
 int32_t main(void)
 {
-
-	fbm320_init();//fbm320 initiation
+	/* fbm320 initiation */
+	fbm320_init();
 
 	while (1)
 	{
-		fbm320_update_data();//Updating fbm320 data
+		/* Updating fbm320 data */
+		fbm320_update_data();
 		if (fbm320_update_rdy) {
-			// real_p = fbm320_read_pressure();//If you need the pressure value read is in uint of Pa, use this function.
-			// real_t = fbm320_read_temperature();//If you need the temperature value read is in unit of degree Celsius, use this function.
-
+			
 			/* This function read pressure and temperature values. Pressure uint:Pa, Temperature unit:0.01 degree Celsius */
 			fbm320_read_data(&real_p, &real_t);
 			fbm320_update_rdy = 0;
